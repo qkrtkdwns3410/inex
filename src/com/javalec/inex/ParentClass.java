8746f31eaf95502b0472984d97bfc02d283ea332
psj <qkrtkdwns3410>
2021-05-10 22:53:05 +0900

    Initial commit

---
+++ src/com/javalec/inex/ParentClass.java
@@ -0,0 +1,20 @@
+package com.javalec.inex;
+
+public class ParentClass {
+    
+    public String pStr = "부모 클래스";
+    
+    public ParentClass() {
+    }
+    
+    public String getPapaName() {
+        System.out.print("홍길동");
+        return "";
+        
+    }
+    
+    public String getMamiName() {
+        System.out.print("홍길순");
+        return "";
+    }
+}
